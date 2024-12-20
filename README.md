ff3a14f8903085d6b01673073d6877d1a9215f70
Jacek Hiszpanski <jacek.hiszpanski@tooploox.com>
2024-12-20 14:21:20 +0100

    update readme

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 	./build.sh
 
+## Start container
+
+    docker run -d --restart unless-stopped -v /home/seafile/seafile:/home/seafile/seafile \
+            -p8000:8000 -p8082:8082 --name seafile  seafile-server
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 docker run -it --rm -v .:/home/seafile/seafile -p8000:8000 -p8082:8082  seafile-server
 
-docker run -it --rm -v /home/seafile/seafile.test:/home/seafile/seafile -p8000:8000 -p8082:8082  seafile-server
-
+docker run -it --rm -v /home/seafile/seafile:/home/seafile/seafile -p8000:8000 -p8082:8082  seafile-server
